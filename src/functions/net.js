dd46e89bcec660de2ef01f25a5531289dfa8d5e2
Francisco Pereira <franciscopere987@gmail.com>
2022-10-24 14:21:17 -0300

    Prueba a main

--- src/functions/net.js
+++ src/functions/net.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import {getAuth, signInWithEmailAndPassword} from 'firebase/auth'
 import '../config/firebase'
 
@@ -8,5 +9,9 @@ export async function login(username, password){
         Then it should call the context
     */
    const auth = getAuth();
-   return await signInWithEmailAndPassword(auth, username, password)
+   const credentials = await signInWithEmailAndPassword(auth, username, password).catch(reason => console.log(reason));
+   console.log(credentials);
+   const result = await axios.get('https://g4-fiuber.herokuapp.com/api/v1/admins/juancitoperez', {headers: {Authorization: `bearer ${await credentials.user.getIdToken()}`}});
+   console.log(result)
+   return credentials;
 }

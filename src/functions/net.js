35ea602f7ec0583aa62f3de2344a82f123da073e
Fran <franciscopere987@gmail.com>
2022-10-25 22:21:45 -0300

    proof of concept main test

--- src/functions/net.js
+++ src/functions/net.js
@@ -3,17 +3,18 @@ import {getAuth, signInWithEmailAndPassword} from 'firebase/auth'
 import '../config/firebase'
 
 
-export async function login(username, password){
+export async function login(mail, password){
     /*
         Should call to firebase to login. 
         Then it should call the context
     */
    const auth = getAuth();
-   const credentials = await signInWithEmailAndPassword(auth, username, password).catch(reason => console.log(reason));
-   console.log(credentials);
-   //const result = await axios.get('http://g4-fiuber.herokuapp.com/api/v1/admins/juancitoperez', {headers: {Authorization: `bearer ${await credentials.user.getIdToken()}`}});
+   const credentials = await signInWithEmailAndPassword(auth, mail, password).catch(reason => console.log(reason));
+   
+   const username = await (await axios.get("https://g4-fiuber.herokuapp.com/api/v1/admins/" + mail)).data
+   
    //const result = await axios.post('https://g4-fiuber.herokuapp.com/api/v1/admins', undefined, 
    //{headers: {"access-control-allow-origin": "*", Authorization: `bearer ${await credentials.user.getIdToken()}`}, data: {username: "Juancitoperez"}});
-   //console.log(result)
+   
    return credentials;
 }

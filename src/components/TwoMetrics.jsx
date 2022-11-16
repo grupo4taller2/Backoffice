8d1472033781e10efb66da140d180449aa5d0896
Francisco Pereira <franciscopere987@gmail.com>
2022-11-16 19:12:42 -0300

    Changed metrics layout

--- src/components/TwoMetrics.jsx
+++ src/components/TwoMetrics.jsx
@@ -6,8 +6,8 @@ export default function TwoMetrics(props){
     return (
         <div className="MetricsDiv">
             <MetricCard title={props.title1} data={props.first} layout={props.layout1} type={props.type1}/>
-            <MetricCard title={props.title3} data={props.third} layout={props.layout3} type={props.type3} />
             <MetricCard title={props.title2} data={props.second} layout={props.layout2} type={props.type2}/>
+            <MetricCard title={props.title3} data={props.third} layout={props.layout3} type={props.type3} />
         </div>
     )
 }

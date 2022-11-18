cc730998be42424aacdcc6535aa61b861b00d3fd
Francisco Pereira <franciscopere987@gmail.com>
2022-11-18 15:36:47 -0300

    Added some last minute changes

--- src/components/ActiveableInput.jsx
+++ src/components/ActiveableInput.jsx
@@ -8,11 +8,16 @@ export function ActivableInput(props){
     const [activeChange, setActiveChange] = React.useState(false);
     const [activeTry, setActiveTry] = React.useState(false);
 
+    const [partialChange, setPartialChange] = React.useState(props.value);
+    const [partialTry, setPartialTry] = React.useState(props.tryValue);
+
     const toggleChange = () => {
+        props.onChange(partialChange)
         setActiveChange(!activeChange)
     };
 
     const toggleTry = () => {
+        props.tryOnChange(partialTry)
         setActiveTry(!activeTry)
     }
 
@@ -23,16 +28,22 @@ export function ActivableInput(props){
     return (
     <div className="ActivableDiv">
         <div className="InputDiv">    
-        <LabeledInput isDisabled={!activeChange} inputClass="CoefficientInput"  id={props.inputClass + "id"}
-        name={props.name} type="text" value={props.value} onChange={props.onChange}
-        invalid={props.failed}/>
+            <LabeledInput isDisabled={!activeChange} inputClass="CoefficientInput"  id={props.inputClass + "id"}
+            name={props.name} type="text" value={partialChange} onChange={setPartialChange}
+            invalid={props.failed}/>
         </div>
         <Button outline color="primary" className="ActiveInputBtn" onClick={toggleChange}>{activeMessage}</Button>
-        <div className="InputDiv">    
-        <LabeledInput isDisabled={!activeTry} inputClass="CoefficientInput"  id={props.inputClass + "id"}
-        name={props.tryName} type="text" value={props.tryValue} onChange={props.tryOnChange}
-        invalid={props.failed}/>
-        </div>
-        <Button outline color="primary" className="ActiveInputBtn" onClick={toggleTry}>{activeTryMessage}</Button>
-    </div>);
+        {
+            props.tryable ?
+            <>
+            <div className="InputDiv">    
+                <LabeledInput isDisabled={!activeTry} inputClass="CoefficientInput"  id={props.inputClass + "id"}
+                name={props.tryName} type="text" value={partialTry} onChange={setPartialTry}
+                invalid={props.failed}/>
+            </div>
+            <Button outline color="primary" className="ActiveInputBtn" onClick={toggleTry}>{activeTryMessage}</Button>
+            </>
+            : 
+            null
+        }</div>);
 }

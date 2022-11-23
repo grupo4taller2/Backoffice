1de75035df92ecacad6a5e3a4b2b854d4ae505a9
Fran <franciscopere987@gmail.com>
2022-11-23 18:35:12 -0300

    Added main front-end functionality for blocking and unblocking users

--- src/components/UserBox.jsx
+++ src/components/UserBox.jsx
@@ -8,15 +8,18 @@ import StatusButton from "./StatusButton";
 
 export default function UserBox(props){
     const [register, setRegister] = React.useState(false);
-
+    const [changeBlockStatus, setChangeBlockStatus] = React.useState(false);
     const [loading, setLoading] = React.useState(false);
 
+    const [blockedStatus, setBlockedStatus] = React.useState(props.isBlocked);
+
     const toggle = props.admin ? () => {} : () => {setRegister(!register)};
+
+    const toggleBlock = () => {setChangeBlockStatus(!changeBlockStatus)};
 
     const context = useUserContext();
 
     const as_admin = props.admin ? () => {} : async () => {
-        console.log("called");
         setLoading(true);
         try{
             await registerAdmin(props.username, context);
@@ -29,16 +32,37 @@ export default function UserBox(props){
         toggle();
     };
 
+    const changeBlockedStatus = async () => {
+        setLoading(true)
+        try{
+            //Await the blocking or unblocking of a user
+            //Hacer props.update()
+        }catch{
+            //Mensaje de error
+        }
+        setLoading(false)
+    }
+
     return (
                     <Card className="FoundUserBox">
                         <InfoUserBox data={props.data}/>
                         {!props.admin && <Button className="AdminBtn" color="primary" outline onClick={toggle}>Make admin</Button>}
+                        {!props.admin && (!props.isBlocked ? 
+                        <Button className="BlockBtn" color="danger" outline onClick={toggleBlock}>Block User</Button>
+                        :
+                        <Button className="BlockBtn" color="success" outline onClick={toggleBlock}>UnBlock User</Button>
+                        )}
                         <Card  outline color="light" className={["UserTypeTag", props.admin ? "AdminBackground" : "RegularBackground"]}>{props.admin ? "Admin" : "Regular user"}</Card>
-                        <Modal isOpen={register} toggle={toggle}>
-                            <ModalHeader>{"Register " + props.username + " as admin ?"}</ModalHeader>
+                        <Modal isOpen={register || changeBlockStatus} toggle={register ? toggle : toggleBlock}>
+                            <ModalHeader>
+                                {  register ? 
+                                "Register " + props.username + " as admin ?" 
+                                :
+                                "Block " + props.username + " ?"}
+                            </ModalHeader>
                             <ModalFooter>
-                                <StatusButton className="AdminRegBtn" onPress={as_admin} color="primary" loading={loading} loadingText="" text="yes" />
-                                <Button color="secondary" onClick={toggle}>
+                                <StatusButton className="AdminRegBtn" onPress={register ? as_admin : changeBlockedStatus} color="primary" loading={loading} loadingText="" text="yes" />
+                                <Button color="secondary" onClick={register ? toggle : toggleBlock}>
                                     Cancel
                                 </Button>
                             </ModalFooter>
